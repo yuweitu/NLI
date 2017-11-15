d6447e14a8cc9bb745febe15e3b7e81b27327cbe
rhan1207 <hanrj1207@yahoo.com>
2017-11-14 22:46:26 -0500

    corrected bugs

--- code/bilstm.py
+++ code/bilstm.py
@@ -223,39 +223,55 @@ class BiLSTM(nn.Module):
         self.dropout = args.dropout
         self.sent_length = 0
         self.word_length = 0
-
+        self.use_cuda = args.cuda
         ### embedding layer 
         self.emb = nn.Embedding(self.num_words+1, self.embed_size, sparse=True)
-        self.emb.weight = Parameter(torch.FloatTensor(emb))  
-
+        self.emb.weight = Parameter(torch.FloatTensor(emb))
+        self.emb.weight.requires_grad = False
+ 
         ### character representation
         self.emb_char = nn.Embedding(self.num_chars+1, self.char_embed_size)
         self.emb_char.weight = Parameter(torch.FloatTensor(self.num_chars+1, self.char_embed_size).uniform_(-1, 1))
  
         self.char_representation = nn.LSTM(self.char_embed_size, self.char_size, self.num_layers, bias = False, bidirectional=False)
+        self.char_hid = self.init_hidden_chars()
 
         ### lstm layer
         self.lstm_s1 = nn.LSTM(self.embed_size+self.char_size, self.hid_size, self.num_layers, bias = False, bidirectional=True)
         self.lstm_s2 = nn.LSTM(self.embed_size+self.char_size, self.hid_size, self.num_layers, bias = False, bidirectional=True)
 
-        # self.s1_hid = self.init_hidden()
-        # self.s2_hid = self.init_hidden()
+        self.s1_hid = self.init_hidden(self.batch_size)
+        self.s2_hid = self.init_hidden(self.batch_size)
+        
         self.pre = PredictionLayer(4*self.hid_size, self.hid_size, self.num_classes, self.dropout)
 
-    def init_hidden(self):
+    def init_hidden(self, batch_size):
         # Before we've done anything, we dont have any hidden state.
         # Refer to the Pytorch documentation to see exactly
         # why they have this dimensionality.
         # The axes semantics are (num_layers, minibatch_size, hidden_dim)
-        return (autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.hid_size)),
-                autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.hid_size)))
+        if self.use_cuda:
+            return (Variable(torch.zeros(self.num_layers*2, batch_size, self.hid_size)).cuda(),
+                    Variable(torch.zeros(self.num_layers*2, batch_size, self.hid_size)).cuda())
+        else:
+            return (Variable(torch.zeros(self.num_layers*2, batch_size, self.hid_size)),
+                    Variable(torch.zeros(self.num_layers*2, batch_size, self.hid_size)))
+    
+    def init_hidden_chars(self):
+        if self.use_cuda:
+            return (Variable(torch.zeros(self.num_layers, self.sent_length, self.char_size)).cuda(),
+                    Variable(torch.zeros(self.num_layers, self.sent_length, self.char_size)).cuda())
+        else:
+            return (Variable(torch.zeros(self.num_layers, self.sent_length, self.char_size)),
+                    Variable(torch.zeros(self.num_layers, self.sent_length, self.char_size)))
+
 
     def forward(self, labels, s1, s2, ch1, ch2):
        
         use_cuda = self.emb.weight.is_cuda
 
-        batch_size = s1.size()[0]
-
+        batch_size = s1.size()[1]
+        
         ### look up the embedding for both sencetences
         s1_emb = self.emb(s1)
         s2_emb = self.emb(s2)
@@ -269,7 +285,8 @@ class BiLSTM(nn.Module):
         all_char1 = []
         for ch in range(ch1.size()[0]):
             ch_emb = self.emb_char(ch1[ch,:,:].squeeze(0))
-            _, s1_chr = self.char_representation(ch_emb)
+            self.char_hid = self.init_hidden_chars()
+            _, s1_chr = self.char_representation(ch_emb, self.char_hid)
             # the first element of s1_chr is the last hidden state
             # we use this as character embedding
             all_char1.append(s1_chr[0].squeeze(0).unsqueeze(1)) 
@@ -282,15 +299,18 @@ class BiLSTM(nn.Module):
         all_char2 = []
         for ch in range(ch2.size()[0]):
             ch_emb = self.emb_char(ch2[ch,:,:].squeeze(0))
-            _, s2_chr = self.char_representation(ch_emb)
+            self.char_hid = self.init_hidden_chars()
+            _, s2_chr = self.char_representation(ch_emb, self.char_hid)
             all_char2.append(s2_chr[0].squeeze(0).unsqueeze(1)) 
 
         all_char2 = torch.cat(all_char2,1)
         
         ### Context Layer
-
-        s1_out, self.s1_hid = self.lstm_s1(torch.cat([s1_emb, all_char1], 2)) #, self.s1_hid)
-        s2_out, self.s2_hid = self.lstm_s2(torch.cat([s2_emb, all_char2], 2)) #, self.s2_hid)
+        
+        self.s1_hid = self.init_hidden(batch_size)
+        self.s2_hid = self.init_hidden(batch_size)
+        s1_out, _ = self.lstm_s1(torch.cat([s1_emb, all_char1], 2), self.s1_hid)
+        s2_out, _ = self.lstm_s2(torch.cat([s2_emb, all_char2], 2), self.s2_hid)
 
         ### Prediction Layer
         pre_list = []
@@ -429,20 +449,20 @@ def main(args):
     
     batch_size = args.batch
  
-    #data = loadData(vocab, args)
     data = loadData(vocab, args)
+    #data = loadData_sample(vocab,10000, args)
     random.shuffle(data)
     
     n_batch = int(np.ceil(len(data)/batch_size))
 
     model = BiLSTM(vocab, vocab_chars, emb, args)
-
+    
     batch = Batch(data, batch_size, vocab, vocab_chars)
      
     if args.cuda:
         model.cuda()
 
-    optimizer = optim.Adagrad(model.parameters(), lr=args.lr)
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
     criterion = nn.CrossEntropyLoss()
     losses = []
     eval_acc_hist = []
@@ -475,12 +495,13 @@ def main(args):
         ave_loss = total_loss/n_batch
         print("average trainning loss is: %s" % str(ave_loss))
         losses.append(ave_loss)
-
+    
         ### evaluate the model after each epoch
         eval_data = loadData(vocab, args, mode = 'eval')
         eval_batch = Batch(eval_data, batch_size, vocab, vocab_chars)
         eval_acc = test_model(model, eval_batch)
         eval_acc_hist.append(eval_acc)
+        print("completed epoch %s, evaluation loss is: %s %%" % (epoch, round(100*eval_acc, 2)))
         
         end_time = time.time()
         print("%s seconds elapsed" % str(end_time - start_time))
@@ -493,17 +514,17 @@ def main(args):
     test_batch = Batch(test_data, batch_size, vocab, vocab_chars)
     test_acc = test_model(model, test_batch)
     print("testing accuracy is: %s %%" % test_acc)
-
+    
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('-emb', type=int, default=300)
     parser.add_argument('-hid', type=int, default=100)
     parser.add_argument('-num_layers', type=int, default=1)
-    parser.add_argument('-batch', type=int, default=500)
+    parser.add_argument('-batch', type=int, default=800)
     parser.add_argument('-epochs', type=int, default=10)
     parser.add_argument('-seed', type=int, default=123)
-    parser.add_argument('-lr', type=float, default =0.05)
+    parser.add_argument('-lr', type=float, default =1)
     parser.add_argument('-num_classes', type=int, default=3)
     parser.add_argument('-dropout', type=float, default=0.1)
 
@@ -548,6 +569,6 @@ if __name__ == '__main__':
 
     args.emb_file = "snli.npy"
     
-    args.cuda = True
+    args.cuda = False
     
     main(args)
